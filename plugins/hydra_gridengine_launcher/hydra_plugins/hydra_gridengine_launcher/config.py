3de47c9cc43ff40839a44876538147d150f10e2d
Yicheng Luo <ethanluoyc@gmail.com>
2021-03-15 21:59:21 +0000

    Fix parameters

--- plugins/hydra_gridengine_launcher/hydra_plugins/hydra_gridengine_launcher/config.py
+++ plugins/hydra_gridengine_launcher/hydra_plugins/hydra_gridengine_launcher/config.py
@@ -6,12 +6,21 @@ from typing import Any, Dict, Optional
 from hydra.core.config_store import ConfigStore
 from hydra_plugins.hydra_submitit_launcher.config import BaseQueueConf
 
-
 @dataclass
-class GridEngineQueueConf(BaseQueueConf):
+class GridEngineQueueConf:
     _target_: str = (
         "hydra_plugins.hydra_gridengine_launcher.submitit_launcher.GridEngineLauncher"
     )
+    submitit_folder: str = "${hydra.sweep.dir}/.submitit/%j"
+    merge = False
+    job_name = 'submitit'
+    num_gpus = 0
+    shell = '/bin/bash'
+    tc = None
+    tmem = '2G'
+    h_vmem = '2G'
+    h_timeout = '00:02:00'
+    
 
 
 ConfigStore.instance().store(
